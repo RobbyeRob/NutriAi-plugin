a750d6e14ad9dc119471cf04d6b7c6352d5f5dd6
Robin Robbins <rockin_ms@hotmail.com>
2024-08-04 17:11:35 -0500

    Create NutriAi-plugin.php
    Adding a file so the I can SRC the plugin.

---
+++ NutriAi-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 
+ * Plugin Name: The Nutri AI
+ * Description: I am your nutritional AI and I am here to help you make good food choices and suggest things that you want.
+ * Version: 1.0.0
+ * Text Domain: options-plugin
+ * 
+ * /
+
+
+if(!defined('ABSPATH')){
+    die('This is not available');
+}
+
+if(!class_exists('NutriAiPlugin')){
+  class NutriAiPlugin{
+      
+      
+  }
+  new NutriAiPlugin;  
+}
