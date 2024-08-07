765bd9ad950f347a44358fc51b90e782c9cac951
Robin Robbins <rockin_ms@hotmail.com>
2024-08-07 11:30:05 -0500

    Adding Extentsions

--- NutriAi-plugin.php
+++ NutriAi-plugin.php
@@ -20,3 +20,12 @@ if(!class_exists('NutriAiPlugin')){
   }
   new NutriAiPlugin;  
 }
+
+/**
+ * 
+ * C:\Program Files\IIS Express\PHP\v8.2\php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ * C:\Program Files\IIS Express\PHP\v8.2\php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+ * C:\Program Files\IIS Express\PHP\v8.2\php composer-setup.php
+ * C:\Program Files\IIS Express\PHP\v8.2\php -r "unlink('composer-setup.php');"
+ * 
+ */
