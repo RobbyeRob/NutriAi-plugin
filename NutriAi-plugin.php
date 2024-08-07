0434b38b3d7957b037bfbf4b1c66f923a4d36e2b
Robin Robbins <rockin_ms@hotmail.com>
2024-08-07 14:10:02 -0500

    Checkin for plugin Delicious Brains.

--- NutriAi-plugin.php
+++ NutriAi-plugin.php
@@ -2,7 +2,7 @@
 /**
  * 
  * Plugin Name: The Nutri AI
- * Description: I am your nutritional AI and I am here to help you make good food choices and suggest things that you want.
+ * Description: I am your nutritional AI and I am here to help you make good food choices and suggest things that you want. Simple questions about the foods you like. Look up your cravings and the AI will provide simple choices for it.
  * Version: 1.0.0
  * Text Domain: options-plugin
  * 
@@ -21,11 +21,3 @@ if(!class_exists('NutriAiPlugin')){
   new NutriAiPlugin;  
 }
 
-/**
- * 
- * C:\Program Files\IIS Express\PHP\v8.2\php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
- * C:\Program Files\IIS Express\PHP\v8.2\php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
- * C:\Program Files\IIS Express\PHP\v8.2\php composer-setup.php
- * C:\Program Files\IIS Express\PHP\v8.2\php -r "unlink('composer-setup.php');"
- * 
- */
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Download the Zip of the branch and extract into your Word Press Plug-ins directory. Please comment with your questions.
+https://wordpress.com/support/plugins/install-a-plugin/
